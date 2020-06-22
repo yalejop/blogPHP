9af7154bb8fa1b58cef0191b354c9bb6dea5e762
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-06-22 17:54:29 -0500

    creación template inicial y conexion base de datos

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+
+include_once 'config.php';
+$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+$query->execute();
+
+$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog con Platzi</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Blog Title</h1>
+        </div>    
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <?php 
+            foreach ($blogPosts as $blogpost) {
+                echo '<div class="blog-post">';
+                echo '<h2>' .  $blogpost['title'] . '</h2>';
+                echo '<p>Jan 1, 2020 by <a href="">Alex</a></p>';
+                echo '<div class="blog-post-image">';
+                echo '<img src="images/keyboard.jpg" alt="imagen avatar">';
+                echo '</div>';
+                echo '<div class="blog-post-content">';
+                echo $blogpost['content'];
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+        <div class="col-md-4">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, accusamus labore sunt culpa ea sequi doloremque optio necessitatibus sint placeat impedit, nemo natus consectetur quas error quia, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, accusamus labore sunt culpa ea sequi doloremque optio necessitatibus sint placeat impedit, nemo natus consectetur quas error quia
+        </div>
+    </div>
+    <div class="row">
+    <footer>
+        this is a footer
+    </footer>
+    </div>
+</div>
+</body>
+</html>
